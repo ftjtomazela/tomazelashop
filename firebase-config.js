3f87fd7e1ef56a59d1fb6ea7cf95c227b5a9b916
ftjtomazela <121243759+ftjtomazela@users.noreply.github.com>
2025-12-20 16:16:30 -0300

    Atualizar o firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Suas configurações do Firebase (Uber Moto Conchas)
 const firebaseConfig = {
   apiKey: "AIzaSyDmtTBGw2ZxtKaUlbD5BlPgwQO26VFyyzU",
   authDomain: "ubermotoconchas.firebaseapp.com",
@@ -9,3 +8,12 @@ const firebaseConfig = {
   appId: "1:697414710628:web:597892e4ba169fa4de7671",
   measurementId: "G-3LF6T0KEVF"
 };
+
+// --- AQUI ESTÁ O SEGREDO QUE FALTAVA ---
+
+// 1. Inicializa o Firebase com as suas chaves
+firebase.initializeApp(firebaseConfig);
+
+// 2. Cria a conexão com o Banco de Dados (Firestore)
+// O 'index.html' e o 'motorista.html' procuram por essa variável 'db'
+const db = firebase.firestore();
