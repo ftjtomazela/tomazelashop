de6699f993e55cfb3086a4249cf3258ecf9db5d2
ftjtomazela <121243759+ftjtomazela@users.noreply.github.com>
2025-12-20 06:17:51 -0300

    Criar firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmtTBGw2ZxtKaUlbD5BlPgwQO26VFyyzU",
+  authDomain: "ubermotoconchas.firebaseapp.com",
+  projectId: "ubermotoconchas",
+  storageBucket: "ubermotoconchas.firebasestorage.app",
+  messagingSenderId: "697414710628",
+  appId: "1:697414710628:web:597892e4ba169fa4de7671",
+  measurementId: "G-3LF6T0KEVF"
+};
