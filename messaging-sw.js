f5c083327b427f618b1947a2b66910e0ff3bb2ca
ftjtomazela <121243759+ftjtomazela@users.noreply.github.com>
2026-01-10 05:13:13 -0300

    Criar messaging-sw.js

---
+++ messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "SUA_API_KEY_AQUI", 
+    projectId: "SEU_PROJECT_ID", 
+    messagingSenderId: "SEU_SENDER_ID", 
+    appId: "SEU_APP_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon-192.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
