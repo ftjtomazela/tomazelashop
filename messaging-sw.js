49b4bace2dc8c78b22c5cf300d53138278b493a6
ftjtomazela <121243759+ftjtomazela@users.noreply.github.com>
2026-01-10 05:14:50 -0300

    Atualizar o messaging-sw.js

--- messaging-sw.js
+++ messaging-sw.js
@@ -2,10 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "SUA_API_KEY_AQUI", 
-    projectId: "SEU_PROJECT_ID", 
-    messagingSenderId: "SEU_SENDER_ID", 
-    appId: "SEU_APP_ID"
+   apiKey: "AIzaSyDmtTBGw2ZxtKaUlbD5BlPgwQO26VFyyzU",
+  authDomain: "ubermotoconchas.firebaseapp.com",
+  projectId: "ubermotoconchas",
+  storageBucket: "ubermotoconchas.firebasestorage.app",
+  messagingSenderId: "697414710628",
+  appId: "1:697414710628:web:597892e4ba169fa4de7671",
+  measurementId: "G-3LF6T0KEVF"
 });
 
 const messaging = firebase.messaging();
